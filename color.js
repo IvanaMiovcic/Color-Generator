38f4635d72cf42b1cd61d9f7f90ee45e769b83f1
IvanaMiovcic <ivanamiovcic@gmail.com>
2021-07-04 21:04:14 -0400

    updated files to add HEX

--- color.js
+++ color.js
@@ -6,12 +6,20 @@ var r = 255;
 var g = 255;
 var b = 255;
 
+var hexR;
+var hexG;
+var hexB;
+
 function changeColor (domElement) {
    r = Math.floor(Math.random() * 255);
    g = Math.floor(Math.random() * 255);
    b = Math.floor(Math.random() * 255);
 
   domElement.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+
+  hexR = r.toString(16);
+  hexG = g.toString(16);
+  hexB = b.toString(16);
 }
 
 buttonElement.addEventListener('click', () => {
@@ -19,5 +27,5 @@ buttonElement.addEventListener('click', () => {
   document.getElementById('red-p').innerHTML = `R: ${r}`;
   document.getElementById('green-p').innerHTML = `G: ${g}`;
   document.getElementById('blue-p').innerHTML = `B: ${b}`;
-
+  document.getElementById('hex').innerHTML = `HEX: #${hexR}${hexG}${hexB}`;
 });
